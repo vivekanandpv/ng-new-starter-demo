a190a933f70844b161dd86fb6ce31105631c23fa
Vivekanand Pujar <6133663+vivekanandpv@users.noreply.github.com>
2026-07-22 11:45:13 +0530

    linting error

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,49 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('ng-new-starter');
+  x = 100;
+
+  // 1. Error: 'userData' is assigned a value but never used
+  // 2. Error: Strings must use single quotes
+  private userData = "John Doe"; 
+
+  // 3. Error: Lifecycle hook should not be empty
+  ngOnInit() {} 
+
+  // 4. Error: Expected a 'for-of' loop instead of a 'for' loop
+  logItems(items: string[]) {
+    for (let i = 0; i < items.length; i++) {
+      console.log(items[i]);
+    }
+  }
+
+  foo() {
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+  }
+
+  bar() {
+console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+  }
 }
